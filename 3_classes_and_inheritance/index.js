51cb7cfa3639ce21b0efc0e14e35d8bc042ac52f
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-06-29 13:44:20 +0300

    add classes

--- 3_classes_and_inheritance/index.js
+++ 3_classes_and_inheritance/index.js
@@ -1,7 +1,3 @@
-# js-practice
-
-## 3. Classes and inheritance
-```JS
 // RootElement <= Box <= instances
 
 class RootElement {
@@ -70,40 +66,3 @@ circle.$el.addEventListener('mouseenter', () => {
 circle.$el.addEventListener('mouseleave', () => {
   circle.show()
 })
-```
-
-![class](src/classes.png)
-
-## 2. Define a method for all arrays (using the prototype)
-```JS
-var a = [1, 2, 3]
-var b = [5, 'Hello', 6]
-
-Array.prototype.double = function() {
-  var newArray = this.map(function(item) {
-    if (typeof item === 'number') {
-      return Math.pow(item, 2)
-    }
-
-    if (typeof item === 'string') {
-      return item += item
-    }
-  })
-
-  return newArray
-}
-
-var newA = a.double()
-var newB = b.double()
-
-console.log('A', newA.double())
-console.log('B', newB)
-````
-
-![obj](src/2_arr.png)
-
-## 1. Creating and configuring an object
-
-https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyDescriptor
-
-![obj](src/1_obj.png)
