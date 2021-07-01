8aa16d253021618218c863908263553bddb9849f
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-07-01 19:09:15 +0300

    update readme with promise implementation

--- 6_promise_implementation/index.js
+++ 6_promise_implementation/index.js
@@ -34,29 +34,75 @@ class myPromise {
     constructor(callback) {
         this.onCatch = null;
         this.onFinally = null;
+        this.thenCbs = [];
+        this.isRejected = false;
 
-        function resolver() {}
+        function resolver(data) {
+            if (this.isRejected) {
+                return;
+            }
+            // here we have to figure out how to call the then's chain
+            this.thenCbs.forEach(cb => {
+                data = cb(data);
+            });
 
-        function rejecter() {}
+            if (this.onFinally) {
+                this.onFinally();
+            }
+        }
 
-        callback(resolver, rejecter);
+        function rejecter(error) {
+            this.isRejected = true;
+            // when we call this method, we create an error that should get into the method catch()
+            // we will find out if we already have processing catch()
+            if (this.onCatch) {
+                // method catch() has already been saved in onCatch var 
+                this.onCatch(error);
+            }
+
+            if (this.onFinally) {
+                this.onFinally();
+            }
+        }
+
+        callback(resolver.bind(this), rejecter.bind(this));
     }
 
     // can be called an infinite number of times
     then(cb) {
-
+        // add our callbacks to an array
+        this.thenCbs.push(cb);
+        // to continue the chain
+        return this;
     }
 
     // we can call this method only 1 time (cause the error is processed only once)
     catch(cb) {
         // that's how we will remember this callback (cb)
         this.onCatch = cb;
+        return this;
     }
 
     // also called only 1 time
     finally (cb) {
         // that's how we will remember this callback (cb)
         this.onFinally = cb;
+        return this;
 
     }
 }
+
+// let's test our functionality
+const promise = new myPromise((resolve, reject) => {
+    setTimeout(() => {
+        // reject('Some error');
+        resolve(10);
+    }, 2000);
+});
+
+promise
+    .then(num => num *= 2)
+    .catch(err => console.error(err))
+    .then(num => num *= 3)
+    .finally(() => console.log('finally'))
+    .then(num => console.log('Done', num));
